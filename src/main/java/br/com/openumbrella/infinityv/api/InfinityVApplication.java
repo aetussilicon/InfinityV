df86086436a5d837a4332140428a2a1dbe6ece7d
Artur <aetussilicon.proton.me>
2023-08-21 11:10:36 -0300

    Primeiro commit, pacotes iniciais

---
+++ src/main/java/br/com/openumbrella/infinityv/api/InfinityVApplication.java
@@ -0,0 +1,13 @@
+package br.com.openumbrella.infinityv.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfinityVApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfinityVApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/openumbrella/infinityv/api/InfinityVApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.openumbrella.infinityv.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfinityVApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
